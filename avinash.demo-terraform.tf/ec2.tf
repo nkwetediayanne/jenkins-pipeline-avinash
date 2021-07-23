b7f1489e0a4c0971424d4cfac872a27a4e86785e
diayanne <diayannen@gmail.com>
2021-07-22 20:37:34 -0600

    my commit

--- avinash.demo-terraform.tf/ec2.tf
+++ avinash.demo-terraform.tf/ec2.tf
@@ -1,8 +1,8 @@
 resource "aws_instance" "diayanneweb" {
   ami_id           ="ami-0aeeebd8d2ab47354"
-  instance_type =t2.micro
+  instance_type ="t2.micro"
 
   tags = {
-    Name =demo-jenkins-avinash
+    Name ="demo-jenkins-avinash"
   }
 }
